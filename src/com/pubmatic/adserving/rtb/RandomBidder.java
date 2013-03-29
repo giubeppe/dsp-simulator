90fe1a5d2cf1083c8276b4e167fe5c29c988b111
giubeppe <giubeppe@gmail.com>
2013-03-29 14:52:23 -0700

    updated to add logging of parameters requested

--- src/com/pubmatic/adserving/rtb/RandomBidder.java
+++ src/com/pubmatic/adserving/rtb/RandomBidder.java
@@ -2,6 +2,9 @@ package com.pubmatic.adserving.rtb;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Enumeration;
+import java.util.Map.Entry;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,11 +12,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
+
 /**
  * Servlet implementation class RandomBidder
  */
 @WebServlet(name = "bidder", description = "A random bidder. Return just empty JSON after a random amount of time", urlPatterns = { "/bidder" })
 public class RandomBidder extends HttpServlet {
+	
+	private static Logger logger = Logger.getAnonymousLogger();
 	private static final double MAX_WAIT_IN_MS = 3000.0;
 	private static final long serialVersionUID = 1L;
        
@@ -33,6 +40,8 @@ public class RandomBidder extends HttpServlet {
 		double maxWaitInMs;
 		try { maxWaitInMs = Double.parseDouble(request.getParameter("maxwait")); } catch (Exception e) { maxWaitInMs = MAX_WAIT_IN_MS; }
 		String reqId = request.getParameter("id");
+
+		printAllParameters(request);
 		
 		long nowInMs = System.currentTimeMillis();
 		long wait = (long) (maxWaitInMs*Math.random()); 
@@ -43,6 +52,17 @@ public class RandomBidder extends HttpServlet {
 		response.setContentType("text/json");
 		Writer writer = response.getWriter();
 		writer.write("{\"debug\": {\"delay\": "+wait+", \"req-id\": \""+reqId+"\", \"max-wait\": "+maxWaitInMs+", \"wait\": "+wait+", \"epoch\": "+nowInMs+"}}");
+	}
+
+	private void printAllParameters(HttpServletRequest request) {
+		
+		for (Entry<String, String[]> entry: request.getParameterMap().entrySet()) {
+			String key = entry.getKey();
+			String[] values = entry.getValue();
+			
+			logger.info("MANDINGO: '"+key+"' => {"+StringUtils.join(values,",")+"}");
+		}
+		
 	}
 
 	/**
